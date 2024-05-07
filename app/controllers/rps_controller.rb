d0d5696c387e6396d2521c65251821561e9f6380
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-07 22:43:56 +0000

    fixed controllers

---
+++ app/controllers/rps_controller.rb
@@ -0,0 +1,21 @@
+class RpsController < ApplicationController
+  def main
+    render ({:template => "main/main_page"})
+  end
+
+  def rock 
+    @game = ["rock","paper","scissors"].sample
+    render ({:template => "game/rock"})
+  
+end
+
+  def paper 
+    @game = ["rock","paper","scissors"].sample
+    render ({:template => "game/paper"})
+  end
+def scissors
+  @game = ["rock", "paper", "scissors"].sample
+  render ({:template => "game/scissors"})
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get("/rock", { :controller => "game", :action => "rock"})
-  
-  get("/paper", { :controller => "game", :action => "paper"})
+get("/", {:controller => "rps", :action => "main"})
 
-  get("/scissors", { :controller => "game", :action => "scissors"})
+get("/rock", {:controller => "rps", :action => "rock"})
 
-  get("/", { :controller => "game", :action => "homepage"})
+get("/paper", {:controller => "rps", :action => "paper"})
+
+get("/scissors", {:controller => "rps", :action => "scissors"})
+
 end
